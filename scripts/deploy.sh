fb82e9e67f8f24fbb2fe3566f6cd647d43425bc1
Kimilm <gnlfla7416@gmail.com>
2022-08-10 19:14:52 +0900

    수정 - ec2 배포 버그 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/app/deploy/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ubuntu/app/deploy/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/deploy/deploy.log
 
